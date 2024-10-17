d176ee168687951ccf1068b549e7392f8d3c4e5b
Luis Mera <profeluismeradev@gmail.com>
2024-10-17 20:56:04 -0300

    clase 9

---
+++ clase-9/src/dao/Users.dao.js
@@ -0,0 +1,26 @@
+import userModel from "./models/User.js";
+
+export default class Users {
+  get = (params) => {
+    return userModel.find(params);
+  };
+
+  getById = (id) => {
+    return userModel.findById(id);
+  };
+
+  save = (doc) => {
+    return userModel.create(doc);
+  };
+  saveMany = (docs) => {
+    return userModel.insertMany(docs);
+  };
+
+  update = (id, doc) => {
+    return userModel.findByIdAndUpdate(id, { $set: doc }, { new: true });
+  };
+
+  delete = (id) => {
+    return userModel.findByIdAndDelete(id);
+  };
+}
